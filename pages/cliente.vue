71adf0171e79893350b32863b0e0be5912e09c80
Federico Cardozo <fedecar94@outlook.com>
2021-09-17 20:43:49 -0400

    feat: added mock de clientes

--- pages/cliente.vue
+++ pages/cliente.vue
@@ -2,15 +2,36 @@
   <v-row align='center' justify='center'>
     <v-col cols='12' md='6' sm='8'>
       <v-card class='py-4 d-flex justify-center'>
-        <h1>Administración de conceptos de uso de puntos</h1>
+        <h1>Administración de datos del cliente</h1>
       </v-card>
       <v-card>
         <v-data-table
           :headers='headers'
           :items='desserts'
+          :single-expand="true"
+          :expanded.sync="expanded"
           :items-per-page='5'
+          item-key="id"
+          show-expand
           class='elevation-1'
         >
+          <template
+            v-slot:item.apellido='{ item }'
+          >
+            {{ item.apellido }}, {{ item.nombre }}
+          </template>
+          <template
+            v-slot:item.documento='{ item }'
+          >
+            {{ item.tipo_doc }} {{ item.documento }}
+          </template>
+          <template v-slot:expanded-item="{ headers, item }">
+            <td :colspan="headers.length">
+              <p>Email: {{item.email}}</p>
+              <p>Nacionalidad: {{item.nacionalidad}}</p>
+              <p>nacimiento: {{item.nacimiento.getDate()}}/{{item.nacimiento.getMonth()}}/{{item.nacimiento.getFullYear()}}</p>
+            </td>
+          </template>
           <template
             v-slot:item._acc='{ item }'
           >
@@ -39,50 +60,152 @@
 
 <script>
 export default {
-  name: 'Parametro',
+  name: 'Cliente',
   data() {
     return {
+      expanded: [],
+      puntos_headers: [
+        { text: 'ID', value: 'id' },
+        { text: 'Apellido/Nombre', value: 'apellido' },
+        { text: 'bolsa', value: 'bolsa' }
+      ],
       headers: [
         { text: 'ID', value: 'id' },
-        { text: 'descripción de concepto', value: 'descripcion' },
-        { text: 'puntos requeridos', value: 'puntos' },
-        { text: 'Acciones', value: '_acc' }
+        { text: 'Apellido/Nombre', value: 'apellido' },
+        { text: 'Documento', value: 'documento' },
+        { text: 'Acciones', value: '_acc' },
+        { text: '', value: 'data-table-expand' },
       ],
       desserts: [
         {
-          id: '1234',
-          descripcion: 'vale de premio',
-          puntos: Math.floor(Math.random() * 100)
+          id: Math.floor(Math.random() * 3000),
+          nombre: 'Jhonny',
+          apellido: 'Test',
+          documento: Math.floor(Math.random() * 8000000),
+          tipo_doc: 'CIP',
+          nacionalidad: 'Paraguayo/a',
+          email: 'test@email.com',
+          nacimiento: new Date((Math.random() * 10000000000) + 1200000000000)
         },
         {
-          id: '1235',
-          descripcion: 'vale de descuento',
-          puntos: Math.floor(Math.random() * 100)
+          id: Math.floor(Math.random() * 3000),
+          nombre: 'Jhonny',
+          apellido: 'Test',
+          documento: Math.floor(Math.random() * 8000000),
+          tipo_doc: 'CIP',
+          nacionalidad: 'Paraguayo/a',
+          email: 'test@email.com',
+          nacimiento: new Date((Math.random() * 10000000000) + 1200000000000)
         },
         {
-          id: '1236',
-          descripcion: 'vale de consumición',
-          puntos: Math.floor(Math.random() * 100)
+          id: Math.floor(Math.random() * 3000),
+          nombre: 'Jhonny',
+          apellido: 'Test',
+          documento: Math.floor(Math.random() * 8000000),
+          tipo_doc: 'CIP',
+          nacionalidad: 'Paraguayo/a',
+          email: 'test@email.com',
+          nacimiento: new Date((Math.random() * 10000000000) + 1200000000000)
         },
         {
-          id: '1237',
-          descripcion: 'canje directo',
-          puntos: Math.floor(Math.random() * 100)
+          id: Math.floor(Math.random() * 3000),
+          nombre: 'Jhonny',
+          apellido: 'Test',
+          documento: Math.floor(Math.random() * 8000000),
+          tipo_doc: 'CIP',
+          nacionalidad: 'Paraguayo/a',
+          email: 'test@email.com',
+          nacimiento: new Date((Math.random() * 10000000000) + 1200000000000)
         },
         {
-          id: '1238',
-          descripcion: 'Concepto 1',
-          puntos: Math.floor(Math.random() * 100)
+          id: Math.floor(Math.random() * 3000),
+          nombre: 'Jhonny',
+          apellido: 'Test',
+          documento: Math.floor(Math.random() * 8000000),
+          tipo_doc: 'CIP',
+          nacionalidad: 'Paraguayo/a',
+          email: 'test@email.com',
+          nacimiento: new Date((Math.random() * 10000000000) + 1200000000000)
         },
         {
-          id: '1239',
-          descripcion: 'Concepto 2',
-          puntos: Math.floor(Math.random() * 100)
+          id: Math.floor(Math.random() * 3000),
+          nombre: 'Jhonny',
+          apellido: 'Test',
+          documento: Math.floor(Math.random() * 8000000),
+          tipo_doc: 'CIP',
+          nacionalidad: 'Paraguayo/a',
+          email: 'test@email.com',
+          nacimiento: new Date((Math.random() * 10000000000) + 1200000000000)
         },
         {
-          id: '1240',
-          descripcion: 'Concepto 3',
-          puntos: Math.floor(Math.random() * 100)
+          id: Math.floor(Math.random() * 3000),
+          nombre: 'Jhonny',
+          apellido: 'Test',
+          documento: Math.floor(Math.random() * 8000000),
+          tipo_doc: 'CIP',
+          nacionalidad: 'Paraguayo/a',
+          email: 'test@email.com',
+          nacimiento: new Date((Math.random() * 10000000000) + 1200000000000)
+        },
+        {
+          id: Math.floor(Math.random() * 3000),
+          nombre: 'Jhonny',
+          apellido: 'Test',
+          documento: Math.floor(Math.random() * 8000000),
+          tipo_doc: 'CIP',
+          nacionalidad: 'Paraguayo/a',
+          email: 'test@email.com',
+          nacimiento: new Date((Math.random() * 10000000000) + 1200000000000)
+        },
+        {
+          id: Math.floor(Math.random() * 3000),
+          nombre: 'Jhonny',
+          apellido: 'Test',
+          documento: Math.floor(Math.random() * 8000000),
+          tipo_doc: 'CIP',
+          nacionalidad: 'Paraguayo/a',
+          email: 'test@email.com',
+          nacimiento: new Date((Math.random() * 10000000000) + 1200000000000)
+        },
+        {
+          id: Math.floor(Math.random() * 3000),
+          nombre: 'Jhonny',
+          apellido: 'Test',
+          documento: Math.floor(Math.random() * 8000000),
+          tipo_doc: 'CIP',
+          nacionalidad: 'Paraguayo/a',
+          email: 'test@email.com',
+          nacimiento: new Date((Math.random() * 10000000000) + 1200000000000)
+        },
+        {
+          id: Math.floor(Math.random() * 3000),
+          nombre: 'Jhonny',
+          apellido: 'Test',
+          documento: Math.floor(Math.random() * 8000000),
+          tipo_doc: 'CIP',
+          nacionalidad: 'Paraguayo/a',
+          email: 'test@email.com',
+          nacimiento: new Date((Math.random() * 10000000000) + 1200000000000)
+        },
+        {
+          id: Math.floor(Math.random() * 3000),
+          nombre: 'Jhonny',
+          apellido: 'Test',
+          documento: Math.floor(Math.random() * 8000000),
+          tipo_doc: 'CIP',
+          nacionalidad: 'Paraguayo/a',
+          email: 'test@email.com',
+          nacimiento: new Date((Math.random() * 10000000000) + 1200000000000)
+        },
+        {
+          id: Math.floor(Math.random() * 3000),
+          nombre: 'Jhonny',
+          apellido: 'Test',
+          documento: Math.floor(Math.random() * 8000000),
+          tipo_doc: 'CIP',
+          nacionalidad: 'Paraguayo/a',
+          email: 'test@email.com',
+          nacimiento: new Date((Math.random() * 10000000000) + 1200000000000)
         },
       ]
     }
